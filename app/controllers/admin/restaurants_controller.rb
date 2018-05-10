61c55f5f8359bc01c54c489b599e3c95af50d61f
Revi <s1492e034@gmail.com>
2018-05-10 12:17:34 +0800

    set up admin authentication

--- app/controllers/admin/restaurants_controller.rb
+++ app/controllers/admin/restaurants_controller.rb
@@ -1,4 +1,7 @@
 class Admin::RestaurantsController < ApplicationController
+  before_action :authenticate_user! #Devise提供的驗證方法
+  before_action :authenticate_admin #自己寫的方法,寫在superclass的ApplicationController中
+
   def index
     
   end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+
+
+private
+
+def authenticate_admin
+  unless current_user.admin? # admin? 方法寫在User model
+    flash[:alert] = "Not allow!"
+    redirect_to root_path
+  end
+end
+
 end
