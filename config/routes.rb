e9c440fb61cd74f394326265a4de6ca50e357fef
Revi <s1492e034@gmail.com>
2018-05-11 09:28:32 +0800

    add restaurants resources for admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users #使用Devise建立User model時產生的
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "restaurants#index"
 
   namespace :admin do
+    resources :restaurants #會產生一組URL Helper和網址 對應到不同的Action
     root "restaurants#index"
   end
 end
