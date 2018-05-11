8a9f9ee49e083d2bb7a051b689ed1510363fa6f4
Revi <s1492e034@gmail.com>
2018-05-11 10:54:29 +0800

    add restaurant new and create actions and views for admin

--- app/controllers/admin/restaurants_controller.rb
+++ app/controllers/admin/restaurants_controller.rb
@@ -5,4 +5,35 @@ class Admin::RestaurantsController < ApplicationController
   def index
     @restaurants = Restaurant.all
   end
+
+  def new
+    @restaurant = Restaurant.new
+    #建立新的餐廳實例@restaurant,然後將這個實例存入變數
+    #Restaurant.new會透過Restaurant Model呼叫new方法,產生一個物件實例,在restaurants table建立新的一列空資料
+    #將新建立的@restaurant送進View樣板,讓使用者填寫表單
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params) #透過params(private method)拿到使用者從表單輸入的資料
+    #Restaurant.new會透過Restaurant Model再次呼叫new方法,把表單資料寫入@restaurant
+    #呼叫save方法將資料存入
+    if @restaurant.save #如果資料成功存入(表示驗證成功)
+      flash[:notice] = "restaurant was successfully created"
+      #flash方法的消息會保存到下一个action，和redirct_to方法一起使用
+      redirect_to admin_restaurants_path #導回後台Index
+    else #驗證失敗
+      flash.now[:alert] = "restaurant was failed to create"
+      #flash.now方法的消息只會在當前視窗顯示，不會保存到下一个action，和render方法一起使用
+      render :new #留在new編輯畫面(render會保留原本有填入的欄位資料)
+    end
+  end
+
+  private
+
+  #Strong Parameters: 在讀取表單資料時，基於安全考量，必須在參數傳入時多做一層處理
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :opening_hours, :tel, :address, :description)
+    #使用require(:restaurant)拿出表單資料
+    #透過permit過濾資料,防止有人傳入不相關的惡意資訊
+  end
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,5 @@
 class Restaurant < ApplicationRecord
+  #加入驗證程序,將name設為必填,若使用者未填寫餐廳名稱,就不會將這筆資料送進資料庫
+  #與資料庫的互動由Model負責,所以寫在Restaurant Model中
+  validates_presence_of :name
 end
