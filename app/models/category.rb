ac1b75e86c6d4fbeb0cbe4b3c756a0b6c2008ac4
Revi <s1492e034@gmail.com>
2018-05-15 16:50:23 +0800

    user can delete category with link on index page

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,6 @@
 class Category < ApplicationRecord
-  validates_presence_of :name
+  validates_presence_of :name #必填欄位
+  validates_uniqueness_of :name #確定新增的分類不會有重複的
 
   has_many :restaurants
 end
