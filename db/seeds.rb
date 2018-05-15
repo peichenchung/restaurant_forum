3f2ba7eaafde82c7bf7a68413d6107f73dd53432
Revi <s1492e034@gmail.com>
2018-05-15 13:27:29 +0800

    setup seed data for default categories and admin user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+# Category
+
+Category.destroy_all #先清空資料表,才不會在重複載入種子資料時,網站出現重複的分類
+
+category_list = [
+  { name: "中式料理" },
+  { name: "日本料理" },
+  { name: "義大利料理" },
+  { name: "墨西哥料理" },
+  { name: "素食料理" },
+  { name: "美式料理" },
+  { name: "複合式料理" }
+]
+
+category_list.each do |category|
+  Category.create( name: category[:name])
+end
+puts "Category created!"
+
+
+# Default admin
+# 建立一個預設的admin帳號
+
+User.create(email: "root@example.com", password: "12345678", role: "admin")
+puts "Default admin created!"
