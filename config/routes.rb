2259016163ca4711b6465e5acd25732205051285
Revi <s1492e034@gmail.com>
2018-05-16 11:35:20 +0800

    setup restaurant resources for public users

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users #使用Devise建立User model時產生的
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  #前台只能瀏覽餐廳資料
+  resources :restaurants, only: [:index, :show]
   root "restaurants#index"
 
   namespace :admin do
