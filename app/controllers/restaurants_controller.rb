48e9697510d678d8de60e75925171b06bba26931
Revi <s1492e034@gmail.com>
2018-05-16 12:25:41 +0800

    complete restaurant index page for public users

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -2,9 +2,9 @@ class RestaurantsController < ApplicationController
   #before_action :authenticate_user!
     #移到application_controller.rb
 
+  #前台首頁
   def index
-    
+    @restaurants = Restaurant.page(params[:page]).per(9)
   end
 
-  private
 end
