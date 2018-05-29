3f1ecc252a51ab9969dd1e38296a43ad3bd17d7c
Revi <s1492e034@gmail.com>
2018-05-29 11:39:46 +0800

    add create action for followship record

--- app/controllers/followships_controller.rb
+++ app/controllers/followships_controller.rb
@@ -1,5 +1,14 @@
 class FollowshipsController < ApplicationController
   def create #follow
-    
+    #產生一個新的Followship物件,並設定兩個外鍵user_id和following_id
+    @followship = current_user.followships.build(following_id: params[:following_id])
+
+    if @followship.save #將攜帶資料的Followship物件存入資料庫
+      flash[:notice] = "Successfully followed"
+      redirect_back(fallback_location: root_path)
+    else
+      flash[:alert] = @followship.errors.full_messages.to_sentence
+      redirect_back(fallback_location: root_path)
+    end
   end
 end
